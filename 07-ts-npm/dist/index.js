9f0c5bc8ba1202432277ba48a132c2499d41193f
Shayane Teixeira <shayaneteixeira56@gmail.com>
2022-07-05 09:48:16 -0300

    Delete 07-ts-npm directory

--- 07-ts-npm/dist/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-let nome = 'Shayane';
-let age = 20;
-console.log(`
-    Nome: ${nome},
-    idade: ${age}
-`);
--- 07-ts-npm/src/index.ts
+++
@@ -1,7 +0,0 @@
-let nome: string = 'Shayane'
-let age: number = 20
-
-console.log(`
-    Nome: ${nome},
-    idade: ${age}
-`)
