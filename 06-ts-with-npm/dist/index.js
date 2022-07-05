2b3f1212471d49911eea8e59062fee6319afa6e5
Shayane Teixeira <shayaneteixeira56@gmail.com>
2022-07-05 09:55:30 -0300

    add folder studies nodejs

---
+++ 06-ts-with-npm/dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let nome = 'Shayane';
+let age = 20;
+console.log(`
+    Nome: ${nome},
+    idade: ${age}
+`);
---
+++ 06-ts-with-npm/src/index.ts
@@ -0,0 +1,7 @@
+let nome: string = 'Shayane'
+let age: number = 20
+
+console.log(`
+    Nome: ${nome},
+    idade: ${age}
+`)
